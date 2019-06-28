67567780191b6feb19586c7f008137c3795b9792
root <root@ip-172-31-41-138.ap-south-1.compute.internal>
2019-06-28 09:17:00 +0000

    version 7.1.2

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>App Version 7.1.1 by GAGAN.</p>
+                <p>App Version 7.1.2 by GAGAN.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
